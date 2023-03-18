380f4b24d9a8ee4e820c5be8b840b44a50d6d2ae
deekshith024 <105255904+deekshith024@users.noreply.github.com>
2023-03-18 13:49:26 -0400

    MenuBar-css-file

--- project_management/src/App.js
+++ project_management/src/App.js
@@ -2,14 +2,20 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
+import MenuBar from './components/MenuBar';
+
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route exact path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
+      <MenuBar />
+      <div>
+        <Routes>
+          <Route exact path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={<div><h1>Welcome to my website!</h1><p>This is the content of my website.</p></div>} />
+        </Routes>
+      </div>
     </Router>
   );
 }
